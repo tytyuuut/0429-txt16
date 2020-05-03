1f2c6e85e0b3b6ba145f81f27e1291f6837c2167
tytyuuut <36351626+tytyuuut@users.noreply.github.com>
2020-05-03 16:31:31 +0800

    Add files via upload

---
+++ vander/vue-main.js
@@ -0,0 +1,204 @@
+// caka產品第一行
+var cakeOne = new Vue({
+	el: "#cakeOne",
+	data: {
+		itemList: [
+			{
+				id: "1",
+				itemName: "巧克力蛋糕",
+				imgUrl: "images/product/cake01.png",
+				price: "900",
+				count: "2",
+				url: "buy-cake-1.html"
+			},
+			{
+				id: "2",
+				itemName: "草莓蛋糕",
+				imgUrl: "images/product/cake02.png",
+				price: "400",
+				count: "2",
+				url: "buy-cake-2.html"
+			},
+			{
+				id: "3",
+				itemName: "檸檬蛋糕",
+				imgUrl: "images/product/cake03.png",
+				price: "700",
+				count: "2",
+				url: "buy-cake-3.html"
+			}
+		]
+	},
+	methods: {},
+	computed: {}
+});
+// caka產品第二行
+var cakeTwo = new Vue({
+	el: "#cakeTwo",
+	data: {
+		itemList: [
+			{
+				id: "1",
+				itemName: "藍莓蛋糕",
+				imgUrl: "images/product/cake04.png",
+				price: "500",
+				count: "2",
+				url: "buy-cake-4.html"
+			},
+			{
+				id: "2",
+				itemName: "草莓蛋糕",
+				imgUrl: "images/product/cake05.png",
+				price: "400",
+				count: "2",
+				url: "buy-cake-5.html"
+			},
+			{
+				id: "3",
+				itemName: "咖啡蛋糕",
+				imgUrl: "images/product/cake06.png",
+				price: "700",
+				count: "2",
+				url: "buy-cake-6.html"
+			}
+		]
+	},
+	methods: {},
+	computed: {}
+});
+// puff產品第ㄧ行
+var puffOne = new Vue({
+	el: "#puffOne",
+	data: {
+		itemList: [
+			{
+				id: "1",
+				itemName: "草莓泡芙",
+				imgUrl: "images/product/puff01.png",
+				price: "500",
+				count: "2",
+				url: "buy-cake-7.html"
+			},
+			{
+				id: "2",
+				itemName: "巧克力泡芙",
+				imgUrl: "images/product/puff02.png",
+				price: "400",
+				count: "2",
+				url: "buy-cake-8.html"
+			},
+			{
+				id: "3",
+				itemName: "法式泡芙",
+				imgUrl: "images/product/puff03.png",
+				price: "700",
+				count: "2",
+				url: "buy-cake-9.html"
+			}
+		]
+	},
+	methods: {},
+	computed: {}
+});
+// puff產品第二行
+var puffTwo = new Vue({
+	el: "#puffTwo",
+	data: {
+		itemList: [
+			{
+				id: "1",
+				itemName: "原味泡芙",
+				imgUrl: "images/product/puff04.png",
+				price: "500",
+				count: "2",
+				url: "buy-cake-10.html"
+			},
+			{
+				id: "2",
+				itemName: "經典泡芙",
+				imgUrl: "images/product/puff05.png",
+				price: "400",
+				count: "2",
+				url: "buy-cake-11.html"
+			},
+			{
+				id: "3",
+				itemName: "繽紛泡芙",
+				imgUrl: "images/product/puff06.png",
+				price: "700",
+				count: "2",
+				url: "buy-cake-12.html"
+			}
+		]
+	},
+	methods: {},
+	computed: {}
+});
+// brand產品第一行
+var brandOne = new Vue({
+	el: "#brandOne",
+	data: {
+		itemList: [
+			{
+				id: "1",
+				itemName: "全麥麵包",
+				imgUrl: "images/product/bread01.png",
+				price: "900",
+				count: "2",
+				url: "buy-cake-13.html"
+			},
+			{
+				id: "2",
+				itemName: "波羅麵包",
+				imgUrl: "images/product/bread02.png",
+				price: "400",
+				count: "2",
+				url: "buy-cake-14.html"
+			},
+			{
+				id: "3",
+				itemName: "巧克力麵包",
+				imgUrl: "images/product/bread03.png",
+				price: "700",
+				count: "2",
+				url: "buy-cake-15.html"
+			}
+		]
+	},
+	methods: {},
+	computed: {}
+});
+// brand產品第二行
+var brandTwo = new Vue({
+	el: "#brandTwo",
+	data: {
+		itemList: [
+			{
+				id: "1",
+				itemName: "經典可頌",
+				imgUrl: "images/product/bread04.png",
+				price: "1500",
+				count: "2",
+				url: "buy-cake-16.html"
+			},
+			{
+				id: "2",
+				itemName: "香芋可頌",
+				imgUrl: "images/product/bread05.png",
+				price: "400",
+				count: "2",
+				url: "buy-cake-17.html"
+			},
+			{
+				id: "3",
+				itemName: "法式麵包",
+				imgUrl: "images/product/bread06.png",
+				price: "700",
+				count: "2",
+				url: "buy-cake-18.html"
+			}
+		]
+	},
+	methods: {},
+	computed: {}
+});
